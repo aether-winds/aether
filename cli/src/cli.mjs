181b76647a703b6ba24ba702874aec494e474f9e
Patrick Ortiz <pogithub+pzychotis@gmail.com>
2024-10-22 22:09:25 -0400

    Added a simple framework for building on a cli tooling system.

--- cli/src/cli.mjs
+++ cli/src/cli.mjs
@@ -2,7 +2,13 @@
 
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import getArgs from 'yargs/yargs';
 
 const commandDir = `${dirname(fileURLToPath(import.meta.url))}/commands`;
+const args = getArgs(process.argv.slice(2));
 
-console.log(commandDir);
+args
+    .commandDir(commandDir, { extensions: ['cjs'] })
+    .demandCommand(1, `Missing command for aw cli.`)
+    .help()
+    .parse();
